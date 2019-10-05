524af953bc348669c0a4f03b0c31b02562abd458
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2019-10-04 21:26:07 -0400

    fix %% escape for logging

--- __init__.py
+++ __init__.py
@@ -1,12 +1,20 @@
 import os
 import logging
 import logging.config
+import configparser
 
 
 def setup_logger():
     root_dir = os.path.abspath(os.path.dirname(__file__))
     log_cfg = os.path.join(root_dir, "setup.cfg")
-    logging.config.fileConfig(log_cfg)
+    cfg = configparser.RawConfigParser()
+    cfg.read([log_cfg])
+    # replace any escaped '%%()', see: 'setup.cfg'
+    for section_name, section in cfg.items():
+        for param, value in section.items():
+            if "%%" in value:
+                cfg.set(section_name, param, value.replace("%%", "%"))
+    logging.config.fileConfig(cfg)
 
 
 setup_logger()
